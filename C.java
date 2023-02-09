c1349e3ae7b1798011a18bca27d2067c7cf3db2f
kiran <kiranresearch2022@gmail.com>
2023-02-09 19:23:25 +0530

    c changes

--- C.java
+++ C.java
@@ -7,8 +7,8 @@ class C {
        System.out.println("in method6 class C");
     }
 
-method12() {
-       System.out.println("in method12 class C");
+method10() {
+       System.out.println("in method10 class C");
     }
 
     
